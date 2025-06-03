d27e28233c28d30ba11278b8f993b5c6681a024f
LivToAir <ynadell06@gmail.com>
2025-06-03 14:17:10 +0300

    FIX #4

--- jellyfin-server/entrypoint.sh
+++ jellyfin-server/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-CONFIG_PATH="/share/jellyfin/config/system.xml"
-CUSTOM_PATH="/share/jellyfin/custom-web"
+# CONFIG_PATH="/share/jellyfin/config/system.xml"
+# CUSTOM_PATH="/share/jellyfin/custom-web"
 
-# Inject WebRootPath if it's missing
-if ! grep -q "<WebRootPath>" "$CONFIG_PATH"; then
-  sed -i "/<ServerConfiguration>/a \ \ \ \ <WebRootPath>$CUSTOM_PATH</WebRootPath>" "$CONFIG_PATH"
-fi
+# # Inject WebRootPath if it's missing
+# if ! grep -q "<WebRootPath>" "$CONFIG_PATH"; then
+#   sed -i "/<ServerConfiguration>/a \ \ \ \ <WebRootPath>$CUSTOM_PATH</WebRootPath>" "$CONFIG_PATH"
+# fi
 
 # Launch Jellyfin
 exec ./jellyfin/jellyfin
